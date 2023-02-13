89749ab94e5ea7dc1656b7d8a0e7153c22707ed2
Ktari ayman <aymanktari31@gmail.com>
2023-02-13 22:50:44 +0100

    user

---
+++ src/user/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDetails } from './interface/user-details.interface';
+import { UserDocument } from './user.schema';
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  getProfileData(user: UserDocument): UserDetails {
+    return {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    };
+  }
+
+  async findByEmail(email: string): Promise<UserDocument | null> {
+    return this.userModel.findOne({ email }).exec();
+  }
+  async findById(id: string): Promise<UserDetails | null> {
+    const user = await this.userModel.findById(id).exec();
+    if (!user) return null;
+    return this.getProfileData(user);
+  }
+  async createUser(
+    name: string,
+    email: string,
+    hashedPassword: string,
+  ): Promise<UserDocument> {
+    const newUser = new this.userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return newUser.save();
+  }
+}
