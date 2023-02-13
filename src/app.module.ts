8ab088a7a9a11e45cfadd153a955ee0ac438ea76
Ktari ayman <aymanktari31@gmail.com>
2023-02-13 22:53:19 +0100

    add imports app.Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InternModule } from './intern/intern.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
   MongooseModule.forRoot('mongodb://localhost:27017/internship'),
-    InternModule],
+    InternModule,
+    UserModule,
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
